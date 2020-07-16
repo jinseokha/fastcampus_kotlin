669d0cf36d7a342f39080c7128a3436261f5b884
david_ha <seok270@gmail.com>
2020-07-16 21:18:39 +0900

    5-3 end

--- app/src/main/java/com/devseok95/fastcampus_kotlin/MainActivity.kt
+++ app/src/main/java/com/devseok95/fastcampus_kotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devseok95.fastcampus_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// android studio tour (20.07.16)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
