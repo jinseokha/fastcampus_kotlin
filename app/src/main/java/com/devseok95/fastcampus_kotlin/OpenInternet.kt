3073d109318f70c2300a255d58e7cca4323d3760
jinseok <seok270@gmail.com>
2020-07-21 18:47:56 +0900

    5-19 end

---
+++ app/src/main/java/com/devseok95/fastcampus_kotlin/OpenInternet.kt
@@ -0,0 +1,43 @@
+package com.devseok95.fastcampus_kotlin
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_open_internet.*
+
+class OpenInternet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_internet)
+
+        open.setOnClickListener {
+            val address = address_edit_text.text.toString()
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+            startActivity(intent)
+        }
+
+        address_edit_text.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0.toString() == "abc") {
+                    Log.d("edit", "text is abc")
+                }
+                Log.d("edit", "afterTextChanged : " + p0) // 3
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("edit", "beforeTextChanged : " + p0) // 1
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("edit", "onTextChanged : " + p0) // 2
+            }
+        })
+        
+
+    }
+}
