ebfbb347119d4e30b83a5ecc1639b4032354ab3a
jinseok <seok270@gmail.com>
2020-07-25 21:38:31 +0900

    5-29 end

---
+++ app/src/main/java/com/devseok95/fastcampus_kotlin/Context.kt
@@ -0,0 +1,16 @@
+package com.devseok95.fastcampus_kotlin
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Context : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_context)
+
+        val context: Context = this
+        val applicationContext  = getApplicationContext()
+
+    }
+}
